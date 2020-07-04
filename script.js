5a19e54a421c9c6927e0fb9a8aaa237cc87e60a4
ryoctrl <ryoctrl@gmail.com>
2020-07-05 05:21:17 +0900

    to be ええ感じ

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ $(function () {
     title.toggleClass("down");
   });
 });
+
+const scrollSpeed = 500;
+
+$(document).ready(function () {
+  new Swiper(".slider", {
+    autoplay: {
+      delay: 5000,
+      stopOnLastSlide: false,
+      disableOnInteraction: false,
+      reverseDirection: false,
+    },
+    speed: 1000,
+    direction: "horizontal",
+    //loop: true,
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  $("#profile_button").click(function () {
+    console.log("profile clicked!");
+    const target = "#profile_wrapper";
+    scrollTo(target);
+    return false;
+  });
+
+  $("#archive_button").click(function () {
+    const target = "#archive_wrapper";
+    scrollTo(target);
+    return false;
+  });
+});
+function scrollTo(targetSelector) {
+  const target = $(targetSelector);
+  const scrollTop = target.offset().top - 70;
+  $("html, body").animate({ scrollTop }, scrollSpeed, "swing");
+}
